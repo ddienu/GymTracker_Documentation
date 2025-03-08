162b36df6afe85cb54b733ec989582109fb4da09
ddienu <rockdann@hotmail.com>
2025-03-08 15:34:14 -0500

    SignIn component finished

--- Mockup/JavaScript/script.js
+++ Mockup/JavaScript/script.js
@@ -1,41 +1,82 @@
 function loadSection(page) {
-    fetch(`/components/${page}.html`)
-        .then(response => 
-            response.text()
-            )
-        .then(html => {
-            document.getElementById("content").innerHTML = html;
-        })
-        .catch(error => console.error("Error al cargar la sección:", error));
+  fetch(`/components/${page}.html`)
+    .then((response) => response.text())
+    .then((html) => {
+      document.getElementById("content").innerHTML = html;
+    })
+    .catch((error) => console.error("Error al cargar la sección:", error));
 }
 
-document.querySelectorAll('.nav-link').forEach(link => {
-    link.addEventListener('click', function(event) {
-        event.preventDefault(); 
-        const section = this.getAttribute('href').substring(1); 
-        loadSection(section); 
-        window.history.pushState({}, "", `#${section}`); 
-    });
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll('#SignInAncor').forEach(btn => {
-        btn.addEventListener('click', function(event)  {
-            event.preventDefault();
-            const section = this.getAttribute('href').substring(1);
-            loadSection(section)
-            window.history.pushState({}, "", `#${section}`); 
-        });
-    });
-});
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    const section = window.location.hash.substring(1) || 'index';
+document.querySelectorAll(".nav-link").forEach((link) => {
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+    const section = this.getAttribute("href").substring(1);
     loadSection(section);
+    window.history.pushState({}, "", `#${section}`);
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("#SignInAncor").forEach((btn) => {
+    btn.addEventListener("click", function (event) {
+      event.preventDefault();
+      const section = this.getAttribute("href").substring(1);
+      loadSection(section);
+      window.history.pushState({}, "", `#${section}`);
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("#RegisterAncor").forEach((btn) => {
+    btn.addEventListener("click", function (event) {
+      event.preventDefault();
+      const section = this.getAttribute("href").substring(1);
+      loadSection(section);
+      window.history.pushState({}, "", `#${section}`);
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.addEventListener("click", function (event) {
+    const usernameElement = document.querySelector("#username");
+    const passwordElement = document.querySelector("#password");
+    if( usernameElement.value.trim() === ""){
+        usernameElement.setCustomValidity("El campo de usuario no puede estar vacio");
+    }else{
+        usernameElement.setCustomValidity("");
+    }
+
+    if( passwordElement.value.trim() === ""){
+        passwordElement.setCustomValidity("El campo de contraseña no puede estar vacio");
+    }else{
+        passwordElement.setCustomValidity("");
+    }
+    
+    if (usernameElement.value.trim() != "" && passwordElement.value.trim() != "") {
+      if (event.target.matches("#js-sign-in-button")) {
+        event.preventDefault();
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "Iniciando sesión",
+          showConfirmButton: false,
+          timerProgressBar: true,
+          timer: 2000,
+        }).then(() => {
+          window.location.href = "#index";
+        });
+      }
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const section = window.location.hash.substring(1) || "index";
+  loadSection(section);
 });
 window.addEventListener("popstate", () => {
-    const section = window.location.hash.substring(1) || 'index';
-    loadSection(section);
+  const section = window.location.hash.substring(1) || "index";
+  loadSection(section);
 });
-
