cfe4845c56a7585d2c8e81b05a4481e0b2c60759
ddienu <rockdann@hotmail.com>
2025-03-10 01:57:45 -0500

    Signup component on dev

--- Mockup/JavaScript/script.js
+++ Mockup/JavaScript/script.js
@@ -1,3 +1,4 @@
+//Method to load the section from the components directory
 function loadSection(page) {
   fetch(`/components/${page}.html`)
     .then((response) => response.text())
@@ -5,6 +6,32 @@ function loadSection(page) {
       document.getElementById("content").innerHTML = html;
     })
     .catch((error) => console.error("Error al cargar la sección:", error));
+}
+
+//Method to validate the passwords
+function validatePassword(password, confirmPassword){
+  if( password === confirmPassword) {
+    document.getElementById("signup-password").setCustomValidity("");
+    document.getElementById("confirmPassword").setCustomValidity("");
+    return true;
+  }else{
+    document.getElementById("signup-password").setCustomValidity("Las contraseñas no coinciden");
+    document.getElementById("confirmPassword").setCustomValidity("Las contraseñas no coinciden");
+    return false;
+  }
+};
+
+function sweetAlertSuccess(message, windowLocation){
+  Swal.fire({
+    position: "center",
+    icon: "success",
+    title: message,
+    showConfirmButton: false,
+    timerProgressBar: true,
+    timer: 2000,
+  }).then(() => {
+    window.location.href = windowLocation;
+  });
 }
 
 document.querySelectorAll(".nav-link").forEach((link) => {
@@ -39,46 +66,58 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.body.addEventListener("click", function (event) {
-    const usernameElement = document.querySelector("#username");
-    const passwordElement = document.querySelector("#password");
-    if( usernameElement.value.trim() === ""){
-        usernameElement.setCustomValidity("El campo de usuario no puede estar vacio");
-    }else{
-        usernameElement.setCustomValidity("");
-    }
-
-    if( passwordElement.value.trim() === ""){
-        passwordElement.setCustomValidity("El campo de contraseña no puede estar vacio");
-    }else{
-        passwordElement.setCustomValidity("");
-    }
-    
-    if (usernameElement.value.trim() != "" && passwordElement.value.trim() != "") {
-      if (event.target.matches("#js-sign-in-button")) {
-        event.preventDefault();
-        Swal.fire({
-          position: "center",
-          icon: "success",
-          title: "Iniciando sesión",
-          showConfirmButton: false,
-          timerProgressBar: true,
-          timer: 2000,
-        }).then(() => {
-          window.location.href = "#index";
-        });
+  document.querySelector("#content").addEventListener("input", function (event) {
+      const usernameElement = document.querySelector("#username");
+      const passwordElement = document.querySelector("#password");
+      if (usernameElement) {
+        if (usernameElement.value.trim() === "") {
+          usernameElement.setCustomValidity("El campo de usuario no puede estar vacio");
+        } else {
+          usernameElement.setCustomValidity("");
+        }
       }
-    }
-  });
-});
 
+      if (passwordElement) {
+        if (passwordElement.value.trim() === "") {
+          passwordElement.setCustomValidity("El campo de contraseña no puede estar vacio");
+        } else {
+          passwordElement.setCustomValidity("");
+        }
+      }
+
+      if (usernameElement && passwordElement) {
+        if (usernameElement.value.trim() != "" && passwordElement.value.trim() != "") {
+          if (event.target.matches("#js-sign-in-button")) {
+            event.preventDefault();
+            sweetAlertSuccess("Iniciando sesión", "#index");
+          }
+        }
+      }
+    });
+});
 function openPopup(){
   window.open(
     '../components/termsAndConditions.html', 
     'Términos y condiciones', 
     'width=600,height=400,top=100,left=100,resizable=no'
   );
-}
+};
+
+// Event to validate passwords
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector("#content").addEventListener('input', function(event) {
+    const signupPassword = document.querySelector("#signup-password").value;
+    const confirmPasswordElement = document.querySelector("#confirmPassword").value;
+    if( signupPassword && confirmPasswordElement){
+      if( validatePassword(signupPassword, confirmPasswordElement) ) { 
+        document.querySelector("#signup-button").addEventListener("click", function(event) {
+          event.preventDefault();
+          sweetAlertSuccess("Cuenta creada exitosamente", '#index');
+        })
+      }
+    }
+    })
+  });
 document.addEventListener("DOMContentLoaded", () => {
   const section = window.location.hash.substring(1) || "index";
   loadSection(section);
