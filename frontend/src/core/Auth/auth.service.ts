c19136499967d3aa475005327115d052b72f2d02
ddienu <rockdann@hotmail.com>
2025-07-04 00:31:08 -0500

    feat: Adding register method to make the request to backend

--- frontend/src/core/Auth/auth.service.ts
+++ frontend/src/core/Auth/auth.service.ts
@@ -1,15 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthModel } from '../../features/auth/model/auth.model';
+import { Login } from '../../features/auth/model/login.model';
+import { Observable } from 'rxjs';
+import { RegisterModel } from '../../features/auth/model/register.model';
+import { RegisterResponse } from '../../features/auth/model/registerResponse.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  apiUrl = "http://localhost:3000/api/auth"
+  private apiUrl = "http://localhost:3000/api/auth";
 
   constructor(private http: HttpClient) { }
 
-  // login
-  
+  login(loginPayload : Login) : Observable<AuthModel>{
+    return this.http.post<AuthModel>(`${this.apiUrl}/login`, loginPayload);
+  }
+
+  register(registerPayload : RegisterModel) : Observable<RegisterResponse>{
+    return this.http.post<RegisterResponse>(`${this.apiUrl}/register`, registerPayload);
+  }
+
 }
