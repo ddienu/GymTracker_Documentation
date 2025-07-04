eee267fee96a9c3298110519f94e8fca47699643
ddienu <rockdann@hotmail.com>
2025-07-04 00:32:23 -0500

    feat: Toastr instalation

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,7 +4,23 @@ import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { ToastrModule } from 'ngx-toastr';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 bootstrapApplication(AppComponent, {
-  providers : [provideHttpClient() , provideRouter(routes)]
+  providers : [provideHttpClient(),
+     provideRouter(routes),
+     importProvidersFrom(
+      BrowserAnimationsModule,
+      ToastrModule.forRoot({
+        timeOut: 3000,
+        positionClass: 'toast-top-right',
+        preventDuplicates: true
+      })
+
+     )
+  ]
 });
